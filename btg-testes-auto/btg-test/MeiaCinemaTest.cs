90a56e871b995dcc243667194fc144f7bff306db
luc0liv <luoliveiradacunha@gmail.com>
2023-12-07 22:03:38 -0300

    test: verifica método que verifica meia entrada e método que encontra motoristas disponíveis

---
+++ btg-testes-auto/btg-test/MeiaCinemaTest.cs
@@ -0,0 +1,29 @@
+﻿using btg_testes_auto;
+
+namespace btg_test
+{
+    public class MeiaCinemaTest
+    {
+        [Theory]
+        [InlineData(true, false, false, false)]
+        [InlineData(false, true, false, false)]
+        [InlineData(false, false, true, true)]
+        public void VerificaSePagaMeia_ReturnsTrue(bool est, bool donor, bool worker, bool contract)
+        {
+            MeiaCinema meiaCinema = new();
+            bool retorno = meiaCinema.VerificarMeiaCinema(est, donor, worker, contract);
+            Assert.True(retorno);
+        }
+
+        [Theory]
+        [InlineData(false, false, false, false)]
+        [InlineData(false, false, false, true)]
+        [InlineData(false, false, true, false)]
+        public void VerificaSeNaoPagaMeia_ReturnsFalse(bool student, bool donor, bool worker, bool contract)
+        {
+            MeiaCinema meiaCinema = new();
+            bool retorno = meiaCinema.VerificarMeiaCinema(student, donor, worker, contract);
+            Assert.False(retorno);
+        }
+    }
+}
---
+++ btg-testes-auto/btg-test/MotoristaTest.cs
@@ -0,0 +1,60 @@
+﻿using btg_testes_auto;
+
+namespace btg_test
+{
+    public class MotoristaTest
+    {
+        Pessoa pessoa1 = new()
+        {
+            Nome = "Fulano",
+            Idade = 20,
+            PossuiHabilitaçãoB = true,
+        };
+        Pessoa pessoa2 = new()
+        {
+            Nome = "Ciclano",
+            Idade = 26,
+            PossuiHabilitaçãoB = true,
+        };
+        Pessoa pessoa3 = new()
+        {
+            Nome = "Beltrano",
+            Idade = 17,
+            PossuiHabilitaçãoB = false,
+        };
+        Pessoa pessoa4 = new()
+        {
+            Nome = "Fulana",
+            Idade = 18,
+            PossuiHabilitaçãoB = false,
+        };
+
+        [Fact]
+        public void EncontrarMotoristas_MotoristasEncontrados_RetornaMensagemComNomesDeMotorista()
+        {
+            List<Pessoa> pessoas = new List<Pessoa>();
+            pessoas.Add(pessoa1);
+            pessoas.Add(pessoa2);
+            pessoas.Add(pessoa3);
+            pessoas.Add(pessoa4);
+
+            Motorista motorista = new();
+            string retorno = motorista.EncontrarMotoristas(pessoas);
+            Assert.Equal($"Uhuu! Os motorista são {pessoas[0].Nome} e {pessoas[1].Nome}", retorno);
+        }
+
+        [Fact]
+        public void EncontrarMotoristas_SemMotoristasDisponiveis_LancaException()
+        {
+            List<Pessoa> pessoas = new List<Pessoa>();
+            pessoas.Add(pessoa3);
+            pessoas.Add(pessoa4);
+
+            Motorista motorista = new();
+            var retorno = motorista.EncontrarMotoristas(pessoas);
+            var exception = Assert.Throws<Exception>(() => retorno);
+
+            Assert.Equal("A viagem não será realizada devido à falta de motoristas!", exception.Message);
+        }
+    }
+}
