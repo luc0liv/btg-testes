6223d63cc179b2c0923d6cd9a543cb79c4b4f593
luc0liv <luoliveiradacunha@gmail.com>
2023-12-08 22:20:38 -0300

    test: preço final do produto

---
+++ btg-testes-auto/btg-test/ImpostoProdutoTest.cs
@@ -0,0 +1,71 @@
+﻿using btg_testes_auto;
+
+namespace btg_test
+{
+    /* ImpostoProduto
+* Uma empresa vende o mesmo produto para diferentes estados.
+* Cada estado possui uma taxa diferente de imposto sobre o produto
+* (MG 7%; SP 12%; RJ 15%; MS 8%; ES 12%; SC 18%;).
+* Faça um programa que retorne o preço final do produto acrescido do imposto do estado em que ele será vendido.
+* Se o estado digitado não for válido, estoure uma exception.
+*/
+    public class ImpostoProdutoTest
+    {
+        [Fact]
+        public void PrecoFinalProduto_RJ()
+        {
+            ImpostoProduto imposto = new(100, "RJ");
+            var result = imposto.PrecoFinalProduto();
+            Assert.Equal(115, result);
+        }
+
+        [Fact]
+        public void PrecoFinalProduto_MG()
+        {
+            ImpostoProduto imposto = new(100, "MG");
+            var result = imposto.PrecoFinalProduto();
+            Assert.Equal(107, result);
+        }
+
+        [Fact]
+        public void PrecoFinalProduto_SP()
+        {
+            ImpostoProduto imposto = new(100, "SP");
+            var result = imposto.PrecoFinalProduto();
+            Assert.Equal(112, result);
+        }
+
+        [Fact]
+        public void PrecoFinalProduto_ES()
+        {
+            ImpostoProduto imposto = new(100, "ES");
+            var result = imposto.PrecoFinalProduto();
+            Assert.Equal(112, result);
+        }
+
+        [Fact]
+        public void PrecoFinalProduto_MS()
+        {
+            ImpostoProduto imposto = new(100, "MS");
+            var result = imposto.PrecoFinalProduto();
+            Assert.Equal(108, result);
+        }
+
+        [Fact]
+        public void PrecoFinalProduto_SC()
+        {
+            ImpostoProduto imposto = new(100, "SC");
+            var result = imposto.PrecoFinalProduto();
+            Assert.Equal(118, result);
+        }
+
+        [Fact]
+        public void PrecoFinalProduto_EstadoInvalido_LancaException()
+        {
+            ImpostoProduto imposto = new(100, "XX");
+            Action act = () => imposto.PrecoFinalProduto();
+            var exception = Assert.Throws<Exception>(act);
+            Assert.Equal("Estado inválido!", exception.Message);
+        }
+    }
+}
---
+++ btg-testes-auto/btg-testes-auto/ImpostoProduto.cs
@@ -0,0 +1,39 @@
+﻿namespace btg_testes_auto
+{
+    public class ImpostoProduto
+    {
+        public decimal PrecoSemImpostos { get; set; }
+        public string Estado { get; set; }
+
+        public ImpostoProduto(decimal preco, string estado)
+        {
+            PrecoSemImpostos = preco;
+            Estado = estado;
+        }
+        public decimal PrecoFinalProduto()
+        {
+            if (Estado == "RJ")
+            {
+                return PrecoSemImpostos * (decimal)1.15;
+            }
+            if  (Estado == "MG")
+            {
+                return PrecoSemImpostos * (decimal)1.07;
+            }
+            if (Estado == "SP" || Estado == "ES")
+            {
+                return PrecoSemImpostos * (decimal)1.12;
+            }
+            if (Estado == "MS")
+            {
+                return PrecoSemImpostos * (decimal)1.08;
+            }
+            if (Estado == "SC")
+            {
+                return PrecoSemImpostos * (decimal)1.18;
+            } 
+
+         throw new Exception("Estado inválido!");
+        }
+    }
+}
