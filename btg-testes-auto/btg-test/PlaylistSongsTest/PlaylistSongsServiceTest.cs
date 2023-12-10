36807200468a6c959c8ceb4201df919a1c35f5e5
luc0liv <luoliveiradacunha@gmail.com>
2023-12-10 02:41:17 -0300

    test: playlist service

---
+++ btg-testes-auto/btg-test/PlaylistSongsTest/PlaylistSongsServiceTest.cs
@@ -0,0 +1,113 @@
+﻿using btg_testes_auto.PlaylistSongs;
+using FluentAssertions;
+using NSubstitute;
+
+namespace btg_test.PlaylistSongsTest
+{
+    public class PlaylistSongsServiceTest
+    {
+        private readonly IPlaylistValidationService _mockPlaylistValidationService;
+        PlaylistService _sut;
+
+        public PlaylistSongsServiceTest()
+        {
+            _mockPlaylistValidationService = Substitute.For<IPlaylistValidationService>();
+            _sut = new(_mockPlaylistValidationService);
+        }
+
+        [Fact]
+        public void AddSongToPlaylist_ReturnsTrue()
+        {
+            Song song = new()
+            {
+                Title = "title",
+                Artist = "Artist"
+            };
+            Song song2 = new()
+            {
+                Title = "title2",
+                Artist = "Artist2"
+            };
+            Playlist playlist = new Playlist()
+            {
+                Songs = new List<Song>(),
+                MaxSongs = 3,
+            };
+
+            playlist.Songs.Add(song);
+            playlist.Songs.Add(song2);
+
+            Song song3 = new()
+            {
+                Title = "title3",
+                Artist = "Artist3"
+            };
+
+            _mockPlaylistValidationService.CanAddSongToPlaylist(playlist, song3)
+                .Returns(true);
+
+            bool result = _sut.AddSongToPlaylist(playlist, song3);
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void AddSongToPlaylist_ReturnsFalse()
+        {
+            Song song = new()
+            {
+                Title = "title",
+                Artist = "Artist"
+            };
+            Song song2 = new()
+            {
+                Title = "title2",
+                Artist = "Artist2"
+            };
+            Playlist playlist = new Playlist()
+            {
+                Songs = new List<Song>(),
+                MaxSongs = 2,
+            };
+
+            playlist.Songs.Add(song);
+            playlist.Songs.Add(song2);
+
+            Song song3 = new()
+            {
+                Title = "title3",
+                Artist = "Artist3"
+            };
+
+            _mockPlaylistValidationService.CanAddSongToPlaylist(playlist, song3)
+                    .Returns(false);
+            var result = _sut.AddSongToPlaylist(playlist, song3);
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void AddSongsToPlaylist_PlaylistContainsSong()
+        {
+            Song song = new()
+            {
+                Title = "title",
+                Artist = "Artist"
+            };
+
+            List<Song> songs = new List<Song>();
+            Playlist playlist = new Playlist()
+            {
+                Songs = new List<Song>(),
+                MaxSongs = 3,
+            };
+
+            songs.Add(song);
+
+            _mockPlaylistValidationService.CanAddSongToPlaylist(playlist, song)
+                .Returns(true);
+
+            _sut.AddSongsToPlaylist(playlist, songs);
+            playlist.Songs.Should().Contain(song);
+        }
+
+    }
+}
