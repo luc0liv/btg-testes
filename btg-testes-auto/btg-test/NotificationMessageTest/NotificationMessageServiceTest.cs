3ff19e2a258c726f336100fcf4ef8ade7dc1e279
luc0liv <luoliveiradacunha@gmail.com>
2023-12-16 12:10:13 -0300

    test: Notification Message

---
+++ btg-testes-auto/btg-test/NotificationMessageTest/NotificationMessageServiceTest.cs
@@ -0,0 +1,69 @@
+﻿using btg_testes_auto.NotificationMessage;
+using FluentAssertions;
+using NSubstitute;
+
+
+namespace btg_test.NotificationEmailTest
+{
+    public class NotificationMessageServiceTest
+    {
+        private readonly IMessageService _mockMessageService;
+        private NotificationService _sut;
+
+        public NotificationMessageServiceTest()
+        {
+            _mockMessageService = Substitute.For<IMessageService>();
+            _sut = new(_mockMessageService);
+        }
+
+        [Fact]
+        public void NotifyUsers_ReturnsTrue()
+        {
+            Notification notification1 = new()
+            {
+                Message = "Messagem 1",
+                UserId = "1"
+            };
+            Notification notification2 = new()
+            {
+                Message = "Messagem 1",
+                UserId = "1"
+            };
+
+            List<Notification> notifications = new() 
+            { notification1, notification2 };
+
+            _mockMessageService.SendMessage(notification1.UserId, notification1.Message)
+                .Returns(true);
+
+            var result = _sut.NotifyUsers(notifications);
+            result.Should().BeTrue();
+            _mockMessageService.Received().SendMessage(Arg.Any<string>(), Arg.Any<string>());
+        }
+
+        [Fact]
+        public void NotifyUsers_ReturnsFalse()
+        {
+            Notification notification1 = new()
+            {
+                Message = "Messagem 1",
+                UserId = "1"
+            };
+            Notification notification2 = new()
+            {
+                Message = "Messagem 1",
+                UserId = "1"
+            };
+
+            List<Notification> notifications = new()
+            { notification1, notification2 };
+
+            _mockMessageService.SendMessage(notification1.UserId, notification1.Message)
+                .Returns(false);
+
+            var result = _sut.NotifyUsers(notifications);
+            result.Should().BeFalse();
+            _mockMessageService.Received().SendMessage(Arg.Any<string>(), Arg.Any<string>());
+        }
+    }
+}
